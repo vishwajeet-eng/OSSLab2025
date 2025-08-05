519a1e3ccc961a1f59e430cb4cc2668029bff909
vishwajeet-eng <231b394@juetguna.in>
2025-08-05 11:15:07 +0530

    Add files via upload

---
+++ GTN.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 42;
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is greater");
+        } else if (num2 > num1) {
+            System.out.println(num2 + " is greater");
+        } else {
+            System.out.println("Both numbers are equal");
+        }
+    }
+}
